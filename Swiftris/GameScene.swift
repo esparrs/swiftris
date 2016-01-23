9bc008f93d873b2215bfe438c78cb146c99b5a9b
esparrs <esparrs@mit.edu>
2016-01-23 15:48:25 -0500

    Array for ticking created

--- Swiftris/GameScene.swift
+++ Swiftris/GameScene.swift
@@ -8,9 +8,52 @@
 
 import SpriteKit
 
+// #1
+let TickLengthLevelOne = NSTimeInterval(600)
+
 class GameScene: SKScene {
+    
+// #2
+    var tick:(() -> ())?
+    var tickLengthMillis = TickLengthLevelOne
+    var lastTick:NSDate?
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("NSCoder not supported")
+    }
+    
+    override init(size: CGSize) {
+        super.init(size: size)
+        
+        anchorPoint = CGPoint(x: 0, y: 1.0)
+        
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 0, y: 0)
+        background.anchorPoint = CGPoint(x: 0, y: 1.0)
+        addChild(background)
+    }
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+// #3
+        guard let lastTick = lastTick else {
+            return
+        }
+        let timePassed = lastTick.timeIntervalSinceNow * -1000.0
+        if timePassed > tickLengthMillis {
+            self.lastTick = NSDate()
+            tick?()
+        }
     }
+    
+// #4
+    func startTicking() {
+        lastTick = NSDate()
+    }
+    
+    func stopTicking() {
+        lastTick = nil
+    }
+    
 }
