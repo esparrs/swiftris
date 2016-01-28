570b944115417c81263ec8ac13e9d2a193a1551a
esparrs <esparrs@mit.edu>
2016-01-28 17:46:53 -0500

    everything else...

--- Swiftris/GameScene.swift
+++ Swiftris/GameScene.swift
@@ -5,18 +5,37 @@
 //  Created by Seve Esparrago on 1/23/16.
 //  Copyright (c) 2016 Bloc. All rights reserved.
 //
+// in method movePreviewShape, there is an error due to nil. No mention of error in tutorial
 
 import SpriteKit
+
+// #7
+// defining point size of each block sprite 
+// in our case 20.0 x 20.0 - the lower of the available resolution options for each block image
+// we declared a layer position which will give us an offset from the edge of the screen
+let BlockSize:CGFloat = 20.0
 
 // #1
 let TickLengthLevelOne = NSTimeInterval(600)
 
 class GameScene: SKScene {
+// #8
+/*
+    SKNodes act as superimposed layers of activity within our scene
+    The gameLayer sits above the background visuals
+    The shapeLayer sits atop the gameLayer
+*/
+    let gameLayer = SKNode()
+    let shapeLayer = SKNode()
+    // this might be what the third comment is refering to in #7
+    let LayerPosition = CGPoint(x: 6, y: -6)
     
 // #2
     var tick:(() -> ())?
     var tickLengthMillis = TickLengthLevelOne
     var lastTick:NSDate?
+    
+    var textureCache = Dictionary<String, SKTexture>()
     
     required init(coder aDecoder: NSCoder) {
         fatalError("NSCoder not supported")
@@ -31,6 +50,23 @@ class GameScene: SKScene {
         background.position = CGPoint(x: 0, y: 0)
         background.anchorPoint = CGPoint(x: 0, y: 1.0)
         addChild(background)
+        
+        addChild(gameLayer)
+        
+        let gameBoardTexture = SKTexture(imageNamed: "gameboard")
+        let gameBoard = SKSpriteNode(texture: gameBoardTexture, size: CGSizeMake(BlockSize * CGFloat(NumColumns), BlockSize * CGFloat(NumRows)))
+        gameBoard.anchorPoint = CGPoint(x:0, y:1.0)
+        gameBoard.position = LayerPosition
+        
+        shapeLayer.position = LayerPosition
+        shapeLayer.addChild(gameBoard)
+        gameLayer.addChild(shapeLayer)
+        
+        runAction(SKAction.repeatActionForever(SKAction.playSoundFileNamed("theme.mp3", waitForCompletion: true)))
+    }
+    
+    func playSound(sound:String) {
+        runAction(SKAction.playSoundFileNamed(sound, waitForCompletion: false))
     }
     
     override func update(currentTime: CFTimeInterval) {
@@ -52,8 +88,143 @@ class GameScene: SKScene {
         lastTick = NSDate()
     }
     
+// #9
+/*
+    This function returns the precise coordinate on the screen for where a block sprite belongs based on its row and column position.
+    We anchor each sprite at its center, so we need to find the center coordinates before placing it in our shapeLayer object.
+*/
+    func pointForColumn(column: Int, row: Int) -> CGPoint {
+        let x = LayerPosition.x + (CGFloat(column) * BlockSize) + (BlockSize / 2)
+        let y = LayerPosition.y - ((CGFloat(row) * BlockSize) + (BlockSize / 2))
+        return CGPointMake(x, y)
+    }
+    
+    func addPreviewShapeToScene(shape:Shape, completion:() -> ()) {
+        for block in shape.blocks {
+// #10
+/*
+            This will add a shape for the first time to the scene as a preview shape.
+            We use a dictionary to store copies of reusable SKTexture objects since each shape will require more than on copy of the same image
+*/
+            var texture = textureCache[block.spriteName]
+            if texture == nil {
+                texture = SKTexture(imageNamed: block.spriteName)
+                textureCache[block.spriteName] = texture
+            }
+            let sprite = SKSpriteNode(texture: texture)
+// #11
+/*
+            We use the pointForColumn(Int, Int) method to place each block's sprite in the proper location.
+            We start it at row - 2 such that the preview piece animates smoothly into place from a higher location
+*/
+            sprite.position = pointForColumn(block.column, row:block.row - 2)
+            shapeLayer.addChild(sprite)
+            block.sprite = sprite
+            
+            // Animation
+            sprite.alpha = 0
+// #12
+/*
+            SKAction objects are responsible for visually manipulating SKNode objects.
+            Each block will fade and move into place as it appears as part of the next piece.
+            It will move two rows down and fade from complete transparency to 70 % opacity.
+            This lets the player ignore the preview piece if they so choose (due to its dullness)
+*/
+            let moveAction = SKAction.moveTo(pointForColumn(block.column, row: block.row), duration: NSTimeInterval(0.2))
+            moveAction.timingMode = .EaseOut
+            let fadeInAction = SKAction.fadeAlphaTo(0.7, duration: 0.4)
+            fadeInAction.timingMode = .EaseOut
+            sprite.runAction(SKAction.group([moveAction, fadeInAction]))
+        }
+        runAction(SKAction.waitForDuration(0.4), completion: completion)
+    }
+    
+/*
+    The remaining two methods make use of the same SKAction objects to move and redraw each block for a given shape
+*/
+    func movePreviewShape(shape:Shape, completion:() -> ()) {
+        for block in shape.blocks {
+            let sprite = block.sprite!
+            let moveTo = pointForColumn(block.column, row:block.row)
+            let moveToAction:SKAction = SKAction.moveTo(moveTo, duration: 0.2)
+            moveToAction.timingMode = .EaseOut
+            sprite.runAction(
+                // hopefully still runs with this errors
+                SKAction.group([moveToAction, SKAction.fadeAlphaTo(1.0, duration: 0.2)]), completion: {})
+        }
+        runAction(SKAction.waitForDuration(0.2), completion: completion)
+    }
+    
+    func redrawShape(shape:Shape, completion:() -> ()) {
+        for block in shape.blocks {
+            let sprite = block.sprite!
+            let moveTo = pointForColumn(block.column, row:block.row)
+            let moveToAction:SKAction = SKAction.moveTo(moveTo, duration: 0.05)
+            moveToAction.timingMode = .EaseOut
+            if block == shape.blocks.last {
+                sprite.runAction(moveToAction, completion: completion)
+            } else {
+                sprite.runAction(moveToAction)
+            }
+        }
+    }
+    
     func stopTicking() {
         lastTick = nil
+    }
+    
+    // #1
+    func animateCollapsingLines(linesToRemove: Array<Array<Block>>, fallenBlocks: Array<Array<Block>>, completion:() -> ()) {
+        var longestDuration: NSTimeInterval = 0
+        // #2
+        for (columnIdx, column) in fallenBlocks.enumerate() {
+            for (blockIdx, block) in column.enumerate() {
+                let newPosition = pointForColumn(block.column, row: block.row)
+                let sprite = block.sprite!
+                // #3
+                let delay = (NSTimeInterval(columnIdx) * 0.05) + (NSTimeInterval(blockIdx) * 0.05)
+                let duration = NSTimeInterval(((sprite.position.y - newPosition.y) / BlockSize) * 0.1)
+                let moveAction = SKAction.moveTo(newPosition, duration: duration)
+                moveAction.timingMode = .EaseOut
+                sprite.runAction(
+                    SKAction.sequence([
+                        SKAction.waitForDuration(delay),
+                        moveAction]))
+                longestDuration = max(longestDuration, duration + delay)
+            }
+        }
+        
+        for rowToRemove in linesToRemove {
+            for block in rowToRemove {
+                // #4
+                let randomRadius = CGFloat(UInt(arc4random_uniform(400) + 100))
+                let goLeft = arc4random_uniform(100) % 2 == 0
+                
+                var point = pointForColumn(block.column, row: block.row)
+                point = CGPointMake(point.x + (goLeft ? -randomRadius : randomRadius), point.y)
+                
+                let randomDuration = NSTimeInterval(arc4random_uniform(2)) + 0.5
+                // #5
+                var startAngle = CGFloat(M_PI)
+                var endAngle = startAngle * 2
+                if goLeft {
+                    endAngle = startAngle
+                    startAngle = 0
+                }
+                let archPath = UIBezierPath(arcCenter: point, radius: randomRadius, startAngle: startAngle, endAngle: endAngle, clockwise: goLeft)
+                let archAction = SKAction.followPath(archPath.CGPath, asOffset: false, orientToPath: true, duration: randomDuration)
+                archAction.timingMode = .EaseIn
+                let sprite = block.sprite!
+                // #6
+                sprite.zPosition = 100
+                sprite.runAction(
+                    SKAction.sequence(
+                        [SKAction.group([archAction, SKAction.fadeOutWithDuration(NSTimeInterval(randomDuration))]),
+                            SKAction.removeFromParent()]))
+            }
+        }
+        // #7
+        runAction(SKAction.waitForDuration(longestDuration), completion:completion)
     }
     
 }
--- Swiftris/GameViewController.swift
+++ Swiftris/GameViewController.swift
@@ -9,10 +9,61 @@
 import UIKit
 import SpriteKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, SwiftrisDelegate, UIGestureRecognizerDelegate {
+    
+    @IBAction func didTap(sender: UITapGestureRecognizer) {
+        swiftris.rotateShape()
+    }
+    
+    @IBAction func didPan(sender: UIPanGestureRecognizer) {
+// #2
+        let currentPoint = sender.translationInView(self.view)
+        if let originalPoint = panPointReference {
+            // #3
+            if abs(currentPoint.x - originalPoint.x) > (BlockSize * 0.9) {
+                // #4
+                if sender.velocityInView(self.view).x > CGFloat(0) {
+                    swiftris.moveShapeRight()
+                    panPointReference = currentPoint
+                } else {
+                    swiftris.moveShapeLeft()
+                    panPointReference = currentPoint
+                }
+            }
+        } else if sender.state == .Began {
+            panPointReference = currentPoint
+        }
+    }
+    
+    @IBAction func didSwipe(sender: UISwipeGestureRecognizer) {
+        swiftris.dropShape()
+    }
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailByGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        if gestureRecognizer is UISwipeGestureRecognizer {
+            if otherGestureRecognizer is UIPanGestureRecognizer {
+                return true
+            }
+        } else if gestureRecognizer is UIPanGestureRecognizer {
+            if otherGestureRecognizer is UITapGestureRecognizer {
+                return true
+            }
+        }
+        return false
+    }
+
     
     var scene: GameScene!
+    var swiftris:Swiftris!
+// #1
+    var panPointReference:CGPoint?
 
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var levelLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,13 +74,106 @@ class GameViewController: UIViewController {
         // Create and configure the scene.
         scene = GameScene(size: skView.bounds.size)
         scene.scaleMode = .AspectFill
+// #13
+/*
+        Set a closure for the tick property of GameScene.swift.
+        Fcuntions are closures with names.
+        We've used a function named didTick() which is definted in #15.
+*/
+        scene.tick = didTick
+        
+        swiftris = Swiftris()
+        swiftris.delegate = self
+        swiftris.beginGame()
         
         // Present the scene.
         skView.presentScene(scene)
-
     }
 
     override func prefersStatusBarHidden() -> Bool {
         return true
+    }
+    
+// #15
+    // Lowers the falling shape by one row and then asks GameScene to redraw the shape at its new location
+    func didTick() {
+        swiftris.letShapeFall()
+    }
+    
+    func nextShape() {
+        let newShapes = swiftris.newShape()
+        guard let fallingShape = newShapes.fallingShape else {
+            return
+        }
+        self.scene.addPreviewShapeToScene(newShapes.nextShape!) {}
+        self.scene.movePreviewShape(fallingShape) {
+// #16
+            self.view.userInteractionEnabled = true
+            self.scene.startTicking()
+        }
+    }
+    
+    func gameDidBegin(swiftris: Swiftris) {
+        levelLabel.text = "\(swiftris.level)"
+        scoreLabel.text = "\(swiftris.score)"
+        scene.tickLengthMillis = TickLengthLevelOne
+        
+        // The following is false when restarting a new game
+        if swiftris.nextShape != nil && swiftris.nextShape!.blocks[0].sprite == nil {
+            scene.addPreviewShapeToScene(swiftris.nextShape!) {
+                self.nextShape()
+            }
+        } else {
+            nextShape()
+        }
+    }
+    
+    func gameDidEnd(swiftris: Swiftris) {
+        view.userInteractionEnabled = false
+        scene.stopTicking()
+        scene.playSound("gameover.mp3")
+        scene.animateCollapsingLines(swiftris.removeAllBlocks(), fallenBlocks: Array<Array<Block>>()) {
+            swiftris.beginGame()
+        }
+    }
+    
+    func gameDidLevelUp(swiftris: Swiftris) {
+        levelLabel.text = "\(swiftris.level)"
+        if scene.tickLengthMillis >= 100 {
+            scene.tickLengthMillis -= 100
+        } else if scene.tickLengthMillis > 50 {
+            scene.tickLengthMillis -= 50
+        }
+        scene.playSound("levelup.mp3")
+    }
+    
+    func gameShapeDidDrop(swiftris: Swiftris) {
+        scene.stopTicking()
+        scene.redrawShape(swiftris.fallingShape!) {
+            swiftris.letShapeFall()
+        }
+        scene.playSound("drop.mp3")
+    }
+    
+    func gameShapeDidLand(swiftris: Swiftris) {
+        scene.stopTicking()
+        self.view.userInteractionEnabled = false
+        // #10
+        let removedLines = swiftris.removeCompletedLines()
+        if removedLines.linesRemoved.count > 0 {
+            self.scoreLabel.text = "\(swiftris.score)"
+            scene.animateCollapsingLines(removedLines.linesRemoved, fallenBlocks:removedLines.fallenBlocks) {
+                // #11
+                self.gameShapeDidLand(swiftris)
+            }
+            scene.playSound("bomb.mp3")
+        } else {
+            nextShape()
+        }
+    }
+    
+// #17
+    func gameShapeDidMove(swiftris: Swiftris) {
+        scene.redrawShape(swiftris.fallingShape!) {}
     }
 }
